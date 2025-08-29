d4b870db83d85f412649fb5cc345c69690d8fe49
kobijaro <kobi.jaro@thecolombiantrip.com>
2025-08-29 15:49:13 -0400

    Add files via upload

---
+++ src:index.js
@@ -0,0 +1,7 @@
+javascript
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PassengerTaskManager from './PassengerTaskManager';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<PassengerTaskManager />);
