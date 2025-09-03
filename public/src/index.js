4134c071abd061bb9d633e796abf199d89d9c5ba
kobijaro <kobi.jaro@thecolombiantrip.com>
2025-09-03 11:31:21 -0400

    Create index.js

---
+++ public/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PassengerTaskManager from './PassengerTaskManager';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <PassengerTaskManager />
+  </React.StrictMode>
+);
